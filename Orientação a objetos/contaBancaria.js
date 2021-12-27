0b050d401dca93c7fbc3eecaece3f2d89f79b216
Gabriel <gabriel.silva.12@aluno.cefet-rj.br>
2021-12-27 17:42:19 -0300

    Orientação a objetos

---
+++ Orientação a objetos/contaBancaria.js
@@ -0,0 +1,84 @@
+class ContaBancaria{
+    constructor(agencia, numero, tipo){
+        this.agencia = agencia
+        this.numero = numero
+        this.tipo = tipo
+        this._saldo = 250
+    }
+    get saldo(){
+        return `Seu saldo é de ${this._saldo} reais`
+    }
+
+    set saldo(valor){
+        this._saldo = valor
+    }
+
+    sacar(valor){
+        if(valor > this._saldo){
+            console.log("Você não tem esse valor na conta!")
+        } 
+        else{
+            this._saldo -= valor
+            console.log(`Foi sacado: ${valor} reais`)
+        }
+    }
+
+    depositar(valor = 0){
+        this._saldo += valor
+        console.log(`Valor depositado: ${valor}`)
+    }
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero)
+        this.tipo = 'Conta corrente'
+        this._cartaoCredito = cartaoCredito
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito
+    }
+
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor
+    }
+
+}
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'Conta poupança'
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'Conta universitária'
+    }
+
+    sacar(valor){
+        if(valor > 500){
+            return 'Operação negada'
+        }
+
+        this._saldo -= valor
+        return `Foi sacado: ${valor} reais`
+
+    }
+}
+
+const gabriel = new ContaBancaria()
+
+
+console.log(gabriel.saldo)
+
+gabriel.sacar(50)
+
+console.log(gabriel.saldo)
+
+gabriel.depositar(100)
+
+console.log(gabriel.saldo)
